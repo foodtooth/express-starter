6a8d0285c1dab49c9adca461a2a426b634e0e4f2
Michael Donux <michaeldx2008@gmail.com>
2017-04-27 17:54:28 +0800

    change swagger structure

--- routes/users/index.js
+++ routes/users/index.js
@@ -6,7 +6,49 @@ let utils = require('../../lib/utils');
 let router = express.Router();
 
 router.route('/')
+/**
+ * @swagger
+ * /users/:
+ *   post:
+ *     description: Create users
+ *     tags:
+ *       - Users
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: user
+ *         description: User object
+ *         in: body
+ *         required: true
+ *         type: string
+ *         schema:
+ *           $ref: '#/definitions/User'
+ *     responses:
+ *       200:
+ *         description: users
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/User'
+ */
 .post(utils.fbController)
+/**
+ * @swagger
+ * /users/:
+ *   get:
+ *     description: Return users
+ *     tags:
+ *       - Users
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: users
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/User'
+ */
 .get(utils.fbController)
 .put(utils.fbController)
 .delete(utils.fbController);
